ad2275275af015be2c0f08300bfe3c4e43a5ef2e
Jthm08 <72540908+Jthm08@users.noreply.github.com>
2021-04-01 16:33:20 +0800

    Update prescription.py

--- prescription/prescription.py
+++ prescription/prescription.py
@@ -3,12 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from os import environ
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/prescriptiondb'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/g1t6_prescription'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/prescription'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # TO RUN SERVICE
-# docker run -p 5003:5000 -e dbURL=mysql+mysqlconnector://is213@host.docker.internal:3306/prescriptiondb jthm/prescription:g1t6
+# docker run -p 5003:5000 -e dbURL=mysql+mysqlconnector://is213@host.docker.internal:3306/g1t6_prescription jthm/prescription:g1t6
  
 db = SQLAlchemy(app)
  
@@ -17,9 +17,7 @@ class Prescription(db.Model):
  
     prescriptionID = db.Column(db.Integer(), primary_key=True)
     medName = db.Column(db.String(25), nullable=False)
-    medDescription = db.Column(db.String(100), nullable=False)
     dosage = db.Column(db.String(20), nullable=False)
-    medCode = db.Column(db.String(8), nullable=False)
     pID = db.Column(db.Integer(), nullable=False)
     aID = db.Column(db.Integer(), nullable=False)
  
@@ -27,9 +25,7 @@ class Prescription(db.Model):
         dto = {
             "prescriptionID": self.prescriptionID, 
             "medName": self.medName, 
-            "medDescription": self.medDescription, 
-            "dosage": self.dosage, 
-            "medCode": self.medCode,
+            "dosage": self.dosage,
             "pID": self.pID, 
             "aID": self.aID
         }
@@ -79,13 +75,11 @@ def find_by_pID(pID):
 def create_prescription():
 
     medName = request.json.get('medName', None)
-    medDescription = request.json.get('medDescription', None)
     dosage = request.json.get('dosage', None)
-    medCode = request.json.get('medCode', None)
     pID = request.json.get('pID', None)
     aID = request.json.get('aID', None)
 
-    prescription = Prescription(medName=medName, medDescription=medDescription, dosage=dosage, medCode=medCode, pID=pID, aID=aID)
+    prescription = Prescription(medName=medName, dosage=dosage, pID=pID, aID=aID)
 
     try:
         db.session.add(prescription)
