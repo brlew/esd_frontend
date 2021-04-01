8056f941b22cc0056a525b399d063cbfe1617181
joyceelynn <yf.tan.2019@sis.smu.edu.sg>
2021-04-01 18:07:15 +0800

    Update app.py

--- login/app.py
+++ login/app.py
@@ -9,60 +9,78 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://is213@localhost:3306/g1t6_logincred'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://is213@localhost:3306/g1t6_logincred'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
 
+class patientlogin(UserMixin, db.Model):
+    pUsername = db.Column(db.String(20), primary_key=True)
+    pPwd = db.Column(db.String(30), nullable=False)
+    
 @login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+def load_user(user_pUsername):
+    return patientlogin.query.get(string(user_pUsername))
+
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    pUsername = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    pPwd = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=80)])
     remember = BooleanField('remember me')
 
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-
+# class RegisterForm(FlaskForm):
+#     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+#     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+#     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user:
+#             if check_password_hash(user.password, form.password.data):
+#                 login_user(user, remember=form.remember.data)
+#                 return redirect(url_for('dashboard'))
+
+#         return '<h1>Invalid username or password</h1>'
+#         #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+#     return render_template('login.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        # return '<h1>' + form.pUsername.data + ' ' + form.pPwd.data + '</h1>'
+        user = patientlogin.query.filter_by(pUsername=form.pUsername.data).first()
         if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('dashboard'))
+            if user.pPwd == form.pPwd.data:
+                return '<h1>' + form.pUsername.data + ' ' + form.pPwd.data + '</h1>'
+            
+            # if check_password_hash(user.pPwd, form.pPwd.data):
+            #     login_user(user, remember=form.remember.data)
+            #     return redirect(url_for('dashboard'))
 
         return '<h1>Invalid username or password</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
-
-    return render_template('login.html', form=form)
+        
+    return render_template('flask_login.html', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        hashed_password = generate_password_hash(form.pPwd.data, method='sha256')
         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
@@ -75,7 +93,7 @@ def signup():
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', name=current_user.username)
+    return render_template('dashboard.html', name=current_user.pUsername)
 
 @app.route('/logout')
 @login_required
