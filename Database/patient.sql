997793bde4a90f8262d6c0558f8b63772fdc5d9a
Jthm08 <72540908+Jthm08@users.noreply.github.com>
2021-04-04 21:28:49 +0800

    Update patient.sql
    Changes to patienttable

--- Database/patient.sql
+++ Database/patient.sql
@@ -19,45 +19,42 @@ CREATE DATABASE IF NOT EXISTS `g1t6_patient` DEFAULT CHARACTER SET utf8 COLLATE 
 USE `g1t6_patient`;
 
 /*--------------------------------------------------------------
-# Table structure for table 'patient'
+# Table structure for table 'patientLogin'
 --------------------------------------------------------------*/
--- name/nric/race/dob/mobile_no
-DROP TABLE IF EXISTS `patient`;
-CREATE TABLE IF NOT EXISTS `patient` (
-  `pID` int(11) NOT NULL AUTO_INCREMENT,
-  `pName` varchar(50) NOT NULL,
-  `pAge` int(11) NOT NULL,
-  `pNumber` varchar(8) NOT NULL,
-  `pAddress` varchar(100) NOT NULL,
-  `pAllergies` varchar(100) NULL,
-  PRIMARY KEY (`pID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+CREATE TABLE `patientLogin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `partialnric` varchar(15) NOT NULL,
+  `race` varchar(15) NOT NULL,
+  `dob` varchar(15) NOT NULL,
+  `mobileno` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- Dumping data for table `patient`
-INSERT INTO `patient` (`pID`, `pName`, `pAge`, `pNumber`, `pAddress`, `pAllergies`) VALUES
-(1, 'Veronica', '21', '98765432', 'cck', 'Paracetamol'),
-(2, 'Jasmine', '21', '87654321', 'amk', 'Cetirizine');
+-- Dumping data for table `patientLogin`
+INSERT INTO `patientLogin` (`id`, `username`, `password`, `email`, `name`, `partialnric`, `race`, `dob`, `mobileno`) VALUES
+(1, 'brlewtest', 'sha256$mGFXdgMi$1e24573419401bb2f72d964c3d4229beddc9ed00355d0683ac31c5a3fab92524', 'brlewtest@brlewtest.com', '', '0', '', '', 0),
+(2, 'seekhealthadmin', 'sha256$cs6f7KtH$dd61931c1ba072e848d7e93eb2e2fe04e0a4f268e4b41c0746400968a4587898', 'seekhealthadmin@f.com', '', '0', '', '', 0),
+(3, 'bwong375', 'sha256$dXmVEk5f$8bbe66ef13d80224223eb14d54f5c5121ca1ab5725a6f1ab10a2bf89e1b44cb1', 'bwong375@gmail.com', 'BERNARD WONG', '*****375C', 'CHINESE', '10 Sep 1948', 97399245),
+(4, 'tanxiaohui98', 'sha256$et6Xf8xb$bde8b74d8aa83c23908e8fcfa137ea4be3267aa327b295289390d4399efa2766', 'tanxiaohui98@gmail.com', 'TAN XIAO HUI', '*****381D', 'CHINESE', '6 Jun 1998', 97399245);
 
 
-/*--------------------------------------------------------------
-# Table structure for table 'medical_records'
---------------------------------------------------------------*/
 DROP TABLE IF EXISTS `medicalRecord`;
 CREATE TABLE IF NOT EXISTS `medicalRecord` (
-  `pID` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `pDiagnosis` varchar(50),
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`created`, `pID`),
-  KEY `medicalRecord_fk1` (`pID`)
+  KEY (`created`, `id`),
+  KEY `medicalRecord_fk1` (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table `medicalRecords`
-INSERT INTO `medicalRecord`(`pID`, `pDiagnosis`) VALUES
-(1, 'Paracetamol');
-
-
--- Foreign Key
-ALTER TABLE `medicalRecord`
-  ADD CONSTRAINT `medicalRecord_fk1` FOREIGN KEY (`pID`) REFERENCES `patient` (`pID`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
+INSERT INTO `medicalRecord`(`id`, `pDiagnosis`) VALUES
+(1, 'Paracetamol'),
+(3, 'Paracetamol'),
+(3, 'Covid'),
+(3, 'Fever'),
+(4, 'Paracetamol');
